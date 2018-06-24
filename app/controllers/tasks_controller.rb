dc040d3076b896221cbd8859d1d93588ea242a88
juri.k <shishi9911@yahoo.co.jp>
2018-06-24 11:03:44 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,30 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
+  
+  def index
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
 
   def create
     @task = current_user.tasks.build(task_params)
@@ -17,7 +41,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
 
   private
@@ -29,7 +53,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_url
+    redirect_to root_url
     end
   end
 end
